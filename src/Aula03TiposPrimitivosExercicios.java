042bde98e825e9421861700a631560a04e85b96d
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-18 15:35:06 -0300

    Exercício 1

---
+++ src/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,14 @@
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String name = "Thor";
+        String  address = "Asgard";
+        double wage = 5500.12;
+        String dateReceived = "18/06/2025";
+
+        System.out.println("I " + name + ", living at the " + address + ", confirm that I have " +
+                "received the salary of " + wage + ", on the date of " + dateReceived + " of juny");
+
+
+    }
+}
