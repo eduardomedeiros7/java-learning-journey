2c0cf95b9959b17fadcd6c846ff7436586002ab5
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-26 11:59:05 -0300

    Operadores de atribuição

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04OperadoresDeAtribuicao.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04OperadoresDeAtribuicao {
+    public static void main(String[] args) {
+
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println("bonus: " + bonus);
+
+    }
+}
