778c95ea93e7ec13843a07dee707625da68f9aa6
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-26 12:15:27 -0300

    Operadores unários

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04OperadoresUnarios.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04OperadoresUnarios {
+    public static void main(String[] args) {
+        int counter = 0;
+        counter += 1;
+        counter++;
+        counter--;
+        ++counter;
+        --counter;
+
+        int counter2 = 0;
+        System.out.println(counter2++);
+        System.out.println(counter2);
+
+        int couonter3 = 0;
+        System.out.println(++couonter3);
+
+        System.out.println("Value of counter: " + counter);
+
+
+    }
+}
