2bc000f916f5fdc9d3bb7a129409d64ba620eb93
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-29 22:34:55 -0300

    Estruturas condicionais (ELSE)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        int age = 17;
+        boolean isAuthorizedBuyDrinks = age >= 18;
+
+        if (isAuthorizedBuyDrinks){
+            System.out.println("Authorized to purchase alcoholic beverages");
+        }else{
+            System.out.println("Not authorized to purchase drinks\"");
+        }
+    }
+        
+    }
