0a92f41b0cdb93b718947828d0931d9d9e4db49f
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-30 21:47:01 -0300

    Desafio de verificar a idade

---
+++ src/desafios/VerificadorDeIdade.java
@@ -0,0 +1,21 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class VerificadorDeIdade {
+    public static void main(String[] args) {
+        int age;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        age = input.nextInt();
+        if (age >=18){
+            System.out.println("You are of legal age ");
+        }else{
+            System.out.println("You do not meet the required age");
+        }
+
+
+
+
+    }
+}
