cd907a44b9c922ac87656d1588a11bc9291fb938
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-01 20:56:59 -0300

    Algoritmo para conferir o status do anluno

---
+++ src/desafiosbasicos/StatusDoAluno.java
@@ -0,0 +1,25 @@
+package desafiosbasicos;
+
+import java.util.Scanner;
+
+
+public class StatusDoAluno {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double grade = 0.0;
+        System.out.println("Enter your grade");
+        grade = input.nextDouble();
+
+        if (grade > 10.0 || grade < 0.0){
+            System.out.println("Invalid Grade");
+        } else if (grade >= 7.0) {
+            System.out.println("Approved");
+        }else if (grade >= 5.0){
+            System.out.println("Recovery");
+        } else {
+            System.out.println("Failed");
+        }
+
+
+    }
+}
