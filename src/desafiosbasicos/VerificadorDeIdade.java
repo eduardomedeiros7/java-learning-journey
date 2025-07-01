6c0edd2054c7ea7ec162824bb7da450fd1323e35
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-06-30 23:12:45 -0300

    Algoritmo para verificar idade

--- src/desafiosbasicos/VerificadorDeIdade.java
+++ src/desafiosbasicos/VerificadorDeIdade.java
@@ -9,7 +9,7 @@ public class VerificadorDeIdade {
         System.out.println("Enter your age:");
         age = input.nextInt();
         if (age >=18){
-            System.out.println("You are of legal age ");
+            System.out.println("You are of legal age");
         }else{
             System.out.println("You do not meet the required age");
         }
