23b34539f74587261383d6676e7d470b1abad6f3
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-08 20:00:50 -0300

    Aprendendo laços de repetição

---
+++ src/desafiosbasicos/ContadorSimples.java
@@ -0,0 +1,12 @@
+package desafiosbasicos;
+
+
+
+public class ContadorSimples {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
