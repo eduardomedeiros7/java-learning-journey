9f2feffe3c1f4fa725562e5f71e33fe561920eaa
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-08 20:12:36 -0300

    Contagem regressiva com FOR

---
+++ src/desafiosbasicos/ContagemRegressiva.java
@@ -0,0 +1,12 @@
+package desafiosbasicos;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+
+        }
+
+    }
+}
