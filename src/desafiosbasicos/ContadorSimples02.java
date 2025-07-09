032e005537a87b59e2158114f1d57296e3b1bbfe
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-09 20:34:44 -0300

    Implementando contador simples com while

---
+++ src/desafiosbasicos/ContadorSimples02.java
@@ -0,0 +1,14 @@
+package desafiosbasicos;
+
+
+public class ContadorSimples02 {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 5){
+            System.out.println(number);
+            number++;
+        }
+    }
+
+}
