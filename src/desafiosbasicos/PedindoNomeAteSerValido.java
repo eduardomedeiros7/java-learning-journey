c911531311c60016efff3523d1dd55b095859f40
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-11 19:23:48 -0300

    Contagem progressiva com validação de entrada com while

--- src/desafiosbasicos/PedindoNomeAteSerValido.java
+++ src/desafiosbasicos/PedindoNomeAteSerValido.java
@@ -1,4 +1,16 @@
 package desafiosbasicos;
 
+import java.util.Scanner;
+
 public class PedindoNomeAteSerValido {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = "";
+        System.out.println("Enter your name");
+        while (name.isEmpty()){
+            System.out.println("please, enter a name valid");
+            name = input.next();
+        }
+        System.out.println("name valid!");
+    }
 }
