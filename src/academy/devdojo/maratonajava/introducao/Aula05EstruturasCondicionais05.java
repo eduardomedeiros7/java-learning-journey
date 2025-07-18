e08eaf00efd8310dcff935144db467aaccd48691
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-18 03:00:44 -0300

    Estudei sobre switch

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,50 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        byte day;
+
+        System.out.println("Enter a day of the week:");
+        day = input.nextByte();
+
+        switch (day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+
+            case 6:
+                System.out.println("Friday");
+                break;
+
+            case 7:
+                System.out.println("Saturday");
+                break;
+
+            default:
+                System.out.println("invalid option");
+        }
+
+        input.close();
+
+
+
+
+    }
+}
