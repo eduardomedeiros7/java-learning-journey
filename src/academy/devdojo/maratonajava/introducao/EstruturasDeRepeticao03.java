8c008a8d1366be16cf677fe10b86cea448576b38
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-18 06:21:54 -0300

    Imprimindo os primeiros 25 números de um valor x

---
+++ src/academy/devdojo/maratonajava/introducao/EstruturasDeRepeticao03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i< 50; i++){
+            if (i > 25){
+                break;
+
+            }
+            System.out.println(i);
+        }
+    }
+}
