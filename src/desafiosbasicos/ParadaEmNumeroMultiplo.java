9f59d3474454cc886133e20e1e013831b7354707
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-24 04:38:57 -0300

    Exemplo de 'break' em laço while para interrupção por condição

--- src/desafiosbasicos/ParadaEmNumeroMultiplo.java
+++ src/desafiosbasicos/ParadaEmNumeroMultiplo.java
@@ -1,4 +1,17 @@
 package desafiosbasicos;
 
 public class ParadaEmNumeroMultiplo {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number <=20){
+            number = number + 1;
+
+            if (number % 7 == 0 ){
+                System.out.println(number + "is a multiple of 7");
+                break;
+            }
+            System.out.println(number);
+        }
+
+    }
 }
