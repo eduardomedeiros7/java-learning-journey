231307220e61e801e564154a566b18b3593e498e
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-26 17:10:02 -0300

    Desafio 2 - Parte 1: Implementa array de String com for-each
    Adiciona a primeira parte da solução para o Desafio 2, demonstrando:
    - Criação e inicialização de um array de String.
    - Uso do laço 'for-each' para iterar e imprimir cada elemento do array.

---
+++ src/desafiosbasicos/ListaDeFrutas.java
@@ -0,0 +1,13 @@
+package desafiosbasicos;
+
+public class ListaDeFrutas {
+    public static void main(String[] args) {
+        String [] fruits =  {"Apple", "Banana", "Grape", "Strawberry", "Pineaplle"};
+
+        for (String fruit : fruits){
+            System.out.println("Fruits: " + fruit);
+
+        }
+
+    }
+}
