bd186161ed8d170552a00ec5baee4f806319e0d8
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-07-29 19:26:58 -0300

    - Demostração básica de array e iteração com for-each Adiciona um exemplo simples de array de inteiros e seu percurso completo usando o laço 'for-each' para imprimir cada elemento

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int [] number = {1, 2, 3, 4,5, 6, 7, 200};
+        for (int num : number){
+            System.out.println(num);
+        }
+
+        }
+    }
+
